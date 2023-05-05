8c93ce93ac823d282bfd5bb3d8c963c87f284892
anslemkelechi <johndoe@example.com>
2023-05-05 20:12:54 +0100

    Revert to old video

--- script.js
+++ script.js
@@ -73,3 +73,45 @@ const rewind = (e) => {
 const forward = (e) => {
   video.currentTime = video.currentTime + (video.duration / 100) * 5;
 };
+
+
+
+  //  <html>
+  //   <head>
+  //   <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/anslemkelechi/CVP/style.min.css" /> 
+  //   </head>
+  //   <body>
+  //       <div class="container">
+  //     <video
+  //       src="${req.protocol}://${req.get(
+  //     "host"
+  //   )}/api/v1/media/video/${videoUrl}" type="video/mp4"
+  //       id="video"
+  //     ></video>
+
+  //     <div class="timeline">
+  //       <div class="bar">
+  //         <div class="inner"></div>
+  //       </div>
+  //     </div>
+  //     <div class="controls">
+  //       <div class="item" onclick="rewind(event)">
+  //         <i class="fa-solid fa-backward"></i>
+  //       </div>
+  //       <div class="item" onclick="play(event)">
+  //         <i class="fa fa-play"></i>
+  //         <i class="fa fa-pause"></i>
+  //       </div>
+  //       <div class="item" onclick="forward(event)">
+  //         <i class="fa-solid fa-forward"></i>
+  //       </div>
+  //       <div class="item"><i class="fa-solid fa-volume-high"></i></div>
+  //       <div class="item item-y">2:00/2:18</div>
+  //       <div class="item item-z" onclick="fullScreen(event)">
+  //         <i class="fa-solid fa-expand"></i>
+  //       </div>
+  //     </div>
+  //   </div>
+  //   <script src="https://cdn.jsdelivr.net/gh/anslemkelechi/CVP/script.min.js"></script>
+  //   </body>
+  //   </html>
